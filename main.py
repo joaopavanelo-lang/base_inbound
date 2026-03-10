c5309dea871efc0ee53d87417257cfb6b13e6214
joaopavanelo-lang <joao.pavanelo@shopee.com>
2026-03-10 14:34:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -74,7 +74,7 @@ async def main():
             # LOGIN
             await page.goto("https://spx.shopee.com.br/")
             await page.wait_for_selector('xpath=//*[@placeholder="Ops ID"]', timeout=15000)
-            await page.locator('xpath=//*[@placeholder="Ops ID"]').fill('Ops182180')
+            await page.locator('xpath=//*[@placeholder="Ops ID"]').fill('Ops292298')
             await page.locator('xpath=//*[@placeholder="Senha"]').fill('@Shopee123')
             await page.locator(
                 'xpath=/html/body/div[1]/div/div[2]/div/div/div[1]/div[3]/form/div/div/button'
